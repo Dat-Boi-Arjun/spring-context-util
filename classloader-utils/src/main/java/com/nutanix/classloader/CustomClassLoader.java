1165cd11d92712ecf7f779da4a2fbafdc2a5a8bc
Kollivakkam Raghavan <raghavan.kollivakkam@nutanix.com>
2020-03-29 20:47:11 -0700

    Remove target.  Add .gitignore entries and fix formatting

--- classloader-utils/src/main/java/com/nutanix/classloader/CustomClassLoader.java
+++ classloader-utils/src/main/java/com/nutanix/classloader/CustomClassLoader.java
@@ -5,32 +5,32 @@ import java.net.URLClassLoader;
 
 public class CustomClassLoader extends URLClassLoader {
 
-    private URL[] indir;
-    private URLClassLoader parentCl;
+  private URL[] indir;
+  private URLClassLoader parentCl;
 
-    public CustomClassLoader(URL[] indir, URLClassLoader parentCl){
-        super(indir);
-        this.indir = indir;
-        this.parentCl = parentCl;
+  public CustomClassLoader(URL[] indir, URLClassLoader parentCl) {
+    super(indir);
+    this.indir = indir;
+    this.parentCl = parentCl;
+  }
+
+  @Override
+  public Class<?> findClass(String name) {
+    Class<?> foundClass = null;
+    try {
+      //need to implement method with input directory
+      URLClassLoader cl = new URLClassLoader(indir);
+      foundClass = cl.loadClass(name);
+
+      if (foundClass == null) {
+        foundClass = super.findClass(name);
+      }
+
     }
+    catch (ClassNotFoundException ignore) {
+    }//we already call the parent class loader if class isn't found
+    return foundClass;
+  }
 
-    @Override
-    public Class<?> findClass(String name){
-        Class<?> foundClass = null;
-        try {
-            //need to implement method with input directory
-            URLClassLoader cl = new URLClassLoader(indir);
-            foundClass = cl.loadClass(name);
 
-            if (foundClass == null) {
-                foundClass = super.findClass(name);
-            }
-
-        } catch(ClassNotFoundException ignore){}//we already call the parent class loader if class isn't found
-        return foundClass;
-    }
-
-    
-
-    
 }
