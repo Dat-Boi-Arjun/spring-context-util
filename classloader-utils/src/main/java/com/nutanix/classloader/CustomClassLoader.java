297ab1108756e1d49d489f14a9f66599a5374657
Arjun Parthasarathy <aparthasarathy23@csus.org>
2020-03-31 22:03:19 -0700

    Covered all error cases for loading in the class

--- classloader-utils/src/main/java/com/nutanix/classloader/CustomClassLoader.java
+++ classloader-utils/src/main/java/com/nutanix/classloader/CustomClassLoader.java
@@ -5,32 +5,28 @@ import java.net.URLClassLoader;
 
 public class CustomClassLoader extends URLClassLoader {
 
-  private URL[] indir;
   private URLClassLoader parentCl;
 
   public CustomClassLoader(URL[] indir, URLClassLoader parentCl) {
     super(indir);
-    this.indir = indir;
     this.parentCl = parentCl;
   }
 
   @Override
-  public Class<?> findClass(String name) {
-    Class<?> foundClass = null;
-    try {
-      //need to implement method with input directory
-      URLClassLoader cl = new URLClassLoader(indir);
-      foundClass = cl.loadClass(name);
-
-      if (foundClass == null) {
+  public Class<?> findClass(String name) throws ClassNotFoundException{
+    //need to implement method with input directory
+    Class<?> foundClass = super.loadClass(name);
+    if (foundClass == null) {
+      try {
         foundClass = super.findClass(name);
       }
-
+      catch (ClassNotFoundException ex){
+        this.parentCl.loadClass(name);
+      }
     }
-    catch (ClassNotFoundException ignore) {
-    }//we already call the parent class loader if class isn't found
-    return foundClass;
+     return foundClass;
   }
 
-
+  
+  
 }
