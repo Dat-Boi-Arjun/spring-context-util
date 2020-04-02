db59cef53ed5286af714b796ddddb4c2d19c80aa
Arjun Parthasarathy <aparthasarathy23@csus.org>
2020-04-02 00:26:28 -0700

    created classloader factory method

--- classloader-utils/src/main/java/com/nutanix/classloader/CustomClassLoader.java
+++ classloader-utils/src/main/java/com/nutanix/classloader/CustomClassLoader.java
@@ -1,6 +1,9 @@
 package com.nutanix.classloader;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
@@ -8,37 +11,50 @@ public class CustomClassLoader extends URLClassLoader {
 
   private ClassLoader parentCl;
 
-  private CustomClassLoader(URL[] indir, ClassLoader parentCl) {
-    super(indir);
+  private CustomClassLoader(URL[] inDir, ClassLoader parentCl) {
+    super(inDir);
     this.parentCl = parentCl;
   }
 
   @SuppressWarnings("WeakerAccess")
-  public static CustomClassLoader classLoaderForPath(String basePath) {
+  public static CustomClassLoader classLoaderForPath(String basePath, ClassLoader parentCl) throws MalformedURLException {
     // collect jar artifacts from this location and then form the URLs to include
     File baseDir = new File(basePath);
+    File[] jars;
+
     // if baseDir is not a directory - throw IllegalArgumentException
+    if (!baseDir.isDirectory()) {
+      throw new IllegalArgumentException();
+    }
 
     // else - get all the jars from this folder. collect them in an array
+    else {
+      jars = baseDir.listFiles((dir, name) -> name.endsWith(".jar"));//tests if it is a jar file
+    }
+
+    URL[] urls = new URL[jars.length];
+
+    for (int i = 0; i < jars.length; i++) {
+      urls[i] = jars[i].toURI().toURL();
+    }
 
     // create a custom classloader instance with these folders in an array and return it.
-
-    return null;
+    return new CustomClassLoader(urls, parentCl);
   }
 
   @Override
-  public Class<?> findClass(String name) throws ClassNotFoundException{
+  public Class<?> findClass(String name) throws ClassNotFoundException {
     //need to implement method with input directory
     Class<?> foundClass = super.findClass(name);
     if (foundClass == null) {
       try {
         foundClass = super.findClass(name);
       }
-      catch (ClassNotFoundException ex){
+      catch (ClassNotFoundException ex) {
         this.parentCl.loadClass(name);
       }
     }
-     return foundClass;
+    return foundClass;
   }
-  
+
 }
