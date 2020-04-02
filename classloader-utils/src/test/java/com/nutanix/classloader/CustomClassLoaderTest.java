5930b526307d27a7f62a391640a293f0afbe56e7
Arjun Parthasarathy <aparthasarathy23@csus.org>
2020-04-02 00:27:50 -0700

    Successfully ran some tests; ClassNotFoundException thrown from others

--- classloader-utils/src/test/java/com/nutanix/classloader/CustomClassLoaderTest.java
+++ classloader-utils/src/test/java/com/nutanix/classloader/CustomClassLoaderTest.java
@@ -3,43 +3,48 @@ package com.nutanix.classloader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+
 public class CustomClassLoaderTest {
 
   private String path1 = System.getProperty("testDir1");
   private String path2 = System.getProperty("testDir2");
 
   @Test
-  public void mustFindClassWhenLoadedByCustomClassLoader() throws ClassNotFoundException {
-    CustomClassLoader cl1 = CustomClassLoader.classLoaderForPath(path1);
-    Assert.assertNotNull(cl1);
-    Class<?> expectedClass = cl1.findClass("main.TestClass1");
+  public void mustFindClassWhenLoadedByCustomClassLoader() throws ClassNotFoundException, MalformedURLException {
+    CustomClassLoader cl2 = CustomClassLoader.classLoaderForPath(path2, ClassLoader.getSystemClassLoader());
+    Assert.assertNotNull(cl2);
+    Class<?> expectedClass = cl2.findClass("TestClass2");
     Assert.assertNotNull(expectedClass);
   }
 
-//  @Test
-//  public void shouldFindDifferentClassesWithDifferentClassLoaders() throws ClassNotFoundException {
-//    Class<?> cl1Class = cl1.findClass("main.TestClass1");
-//    Class<?> cl2Class = cl2.findClass("main.TestClass1");
-//    Assert.assertNotSame(cl1Class, cl2Class);
-//
-//  }
-//
-//  @Test(expectedExceptions = ClassNotFoundException.class)
-//  public void shouldNotFindClassesWithGivenDirectory() throws ClassNotFoundException {
-//    cl1.findClass("main.TestClass2");
-//    cl2.findClass("main.TestClass1");
-//  }
-//
-//  @Test
-//  public void allClassLoadersLoadTheSameClass() throws ClassNotFoundException, MalformedURLException {
-//    URL[] baseDir = {new File(System.getProperty("baseDir")).toURI().toURL()};
-//    Class<?> ClassV1 = Class.forName(this.getClass().getName());
-//    Class<?> ClassV2 = Class.forName(
-//        this.getClass().getName(),
-//        true, new CustomClassLoader(baseDir, new URLClassLoader(baseDir)));
-//    Class<?> ClassV3 = Class.forName(this.getClass().getName(), true, ClassLoader.getSystemClassLoader());
-//
-//    Assert.assertSame(ClassV1, ClassV2);
-//    Assert.assertSame(ClassV2, ClassV3);
-//  }
+  @Test
+  public void shouldFindDifferentClassesWithDifferentClassLoaders() throws ClassNotFoundException, MalformedURLException {
+    CustomClassLoader cl1 = CustomClassLoader.classLoaderForPath(path1, ClassLoader.getSystemClassLoader());
+    CustomClassLoader cl2 = CustomClassLoader.classLoaderForPath(path2, ClassLoader.getSystemClassLoader());
+    Class<?> cl1Class = cl1.findClass("TestClass1");
+    Class<?> cl2Class = cl2.findClass("TestClass2");
+    Assert.assertNotSame(cl1Class, cl2Class);
+
+  }
+
+  @Test(expectedExceptions = ClassNotFoundException.class)
+  public void shouldNotFindClassesWithGivenDirectory() throws ClassNotFoundException, MalformedURLException {
+    CustomClassLoader cl1 = CustomClassLoader.classLoaderForPath(path1, ClassLoader.getSystemClassLoader());
+    CustomClassLoader cl2 = CustomClassLoader.classLoaderForPath(path2, ClassLoader.getSystemClassLoader());
+    cl1.findClass("TestClass2");
+    cl2.findClass("TestClass1");
+  }
+
+  @Test
+  public void allClassLoadersLoadTheSameClass() throws ClassNotFoundException, MalformedURLException {
+    Class<?> ClassV1 = Class.forName(this.getClass().getName());
+    Class<?> ClassV2 = Class.forName(
+        this.getClass().getName(),
+        true, CustomClassLoader.classLoaderForPath(System.getProperty("baseDir"), ClassLoader.getSystemClassLoader()));
+    Class<?> ClassV3 = Class.forName(this.getClass().getName(), true, ClassLoader.getSystemClassLoader());
+
+    Assert.assertSame(ClassV1, ClassV2);
+    Assert.assertSame(ClassV2, ClassV3);
+  }
 }
