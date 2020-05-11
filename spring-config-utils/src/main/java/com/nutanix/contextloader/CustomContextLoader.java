f9985c8de2e4450ed58108b735af179995e1cf8b
Arjun Parthasarathy <aparthasarathy23@csus.org>
2020-05-10 23:37:38 -0700

    implemented context loader
    Still has error with lombok annotation not recognized

--- spring-config-utils/src/main/java/com/nutanix/contextloader/CustomContextLoader.java
+++ spring-config-utils/src/main/java/com/nutanix/contextloader/CustomContextLoader.java
@@ -1,27 +1,47 @@
 package com.nutanix.contextloader;
 
 import com.nutanix.classloader.CustomClassLoader;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.net.MalformedURLException;
 
+@Slf4j
 public class CustomContextLoader{
 
-  private ApplicationContext applicationContext;
-  private String baseDir;//will be in separate modules
+  private DefaultListableBeanFactory beanFactory;
+  private AnnotationConfigApplicationContext parentCtx;
 
-  public CustomContextLoader() {
-    //take in fixture data
+  public CustomContextLoader(DefaultListableBeanFactory beanFactory, AnnotationConfigApplicationContext parentCtx) {
+    this.beanFactory = beanFactory;
+    this.parentCtx = parentCtx;
   }
 
-  public ApplicationContext getApplicationContext() {
-    return applicationContext;
-  }
+  public AnnotationConfigApplicationContext loadContext(String classpath) {
+    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(beanFactory);
+    try {
+      ctx.setParent(this.parentCtx);
+      ctx.setClassLoader(CustomClassLoader.classLoaderForPath(classpath, ClassLoader.getSystemClassLoader()));
+      ctx.refresh();
+    }
+    catch (MalformedURLException e) {
+      log.error("Failed", e);
+    }
 
-  private Class<?> loadBean() throws MalformedURLException, ClassNotFoundException {
-    CustomClassLoader cl = CustomClassLoader.classLoaderForPath(baseDir, ClassLoader.getSystemClassLoader());
-    Class<?> bean = cl.findClass("name of bean class");
-    return bean;
+    return ctx;
   }
+    /*
+    Tests:
+    loadBeans
+    findBeansFromMulitpleConfigClass
+    getBeanfromContext
+    findParentBeanFromChild
+     */
+
+
+
+
 
 }
